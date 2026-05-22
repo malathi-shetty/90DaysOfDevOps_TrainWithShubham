bd471c2973f684ab02b685b09bd1297d8b0cec5b
Malathi Shetty <113874244+malathi-shetty@users.noreply.github.com>
2026-05-23 00:52:02 +0530

    Update index.js

--- 2026/day-45/src/index.js
+++ 2026/day-45/src/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 /* Serve static frontend */
@@ -10,22 +9,18 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 /* Health endpoint */
 app.get('/health', (req, res) => {
-
   res.json({
     status: 'healthy',
     commit: process.env.GIT_SHA || 'local',
     buildDate: process.env.BUILD_DATE || 'local'
   });
-
 });
 
-/* Default route */
+/* API route */
 app.get('/api', (req, res) => {
-
   res.json({
     message: 'Docker CI/CD Pipeline Running Successfully 🚀'
   });
-
 });
 
 app.listen(PORT, () => {
