5e9006fb5e31270be149cf3c3db2a7a069062ae7
shettymalathi113 <shettymalathi113@gmail.com>
2026-06-24 14:20:12 +0000

    day-76-otel-alerting-files

---
+++ 2026/day-76/metrics-enabled-app/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from prometheus_client import Counter, generate_latest
+from prometheus_client import CONTENT_TYPE_LATEST
+from prometheus_client import Counter, start_http_server
+
+REQUEST_COUNT = Counter('test_requests_total', 'Total requests')
+
+@app.route("/")
+def home():
+    REQUEST_COUNT.inc()
+    return "Hello"
+
+app = Flask(__name__)
+
+requests_total = Counter(
+    "app_requests_total",
+    "Total requests received"
+)
+
+@app.route("/")
+def home():
+    requests_total.inc()
+    return "Hello from Prometheus Demo!"
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {
+        "Content-Type": CONTENT_TYPE_LATEST
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
